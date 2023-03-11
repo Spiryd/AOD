3a327bb32f44b3175f5bab0b0b89104caa553b27
Spiryd <neumann.maks@gmail.com>
2023-03-11 11:51:16 +0100

    cargo setup

---
+++ lista_1/target/debug/deps/lista_1-9f7734aa14fcf9fc.d
@@ -0,0 +1,5 @@
+/Volumes/T7/AOD/lista_1/target/debug/deps/lista_1-9f7734aa14fcf9fc.rmeta: src/main.rs
+
+/Volumes/T7/AOD/lista_1/target/debug/deps/lista_1-9f7734aa14fcf9fc.d: src/main.rs
+
+src/main.rs:
---
+++ lista_1/target/debug/deps/lista_1-b7ec07bb9b8861aa.d
@@ -0,0 +1,5 @@
+/Volumes/T7/AOD/lista_1/target/debug/deps/lista_1-b7ec07bb9b8861aa.rmeta: src/main.rs
+
+/Volumes/T7/AOD/lista_1/target/debug/deps/lista_1-b7ec07bb9b8861aa.d: src/main.rs
+
+src/main.rs:
