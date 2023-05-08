0e3631dbbc92e55ce4aacc4a6c2237476e59ae22
Spiryd <neumann.maks@gmail.com>
2023-05-08 21:23:29 +0200

    data ingest and basic optimizations

--- lista_3/src/lib.rs
+++ lista_3/src/lib.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, BinaryHeap, HashSet};
+use std::collections::{BinaryHeap, HashSet};
 use std::cmp::Ordering;
 use std::cmp::Reverse;
 
@@ -12,7 +12,7 @@ impl Ord for SearchNode {
     fn cmp(&self, other: &Self) -> Ordering {
         other.distance.cmp(&self.distance)
     }
-}
+}   
 
 impl PartialOrd for SearchNode {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
@@ -20,43 +20,49 @@ impl PartialOrd for SearchNode {
     }
 }
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, Default)]
 pub struct Graph{
     node_quantity: usize,
     max_weight: usize,
-    adj: HashMap<usize, HashSet<(usize, usize)>>
+    adj: Vec<HashSet<(usize, usize)>>
 }
 
 impl Graph {
-    pub fn new(node_quantity: usize, max_weight: usize) -> Self{
-        let adj: HashMap<usize, HashSet<(usize, usize)>> = HashMap::new();
+    pub fn new(node_quantity: usize) -> Self{
+        let adj: Vec<HashSet<(usize, usize)>> = vec![HashSet::new(); node_quantity];
         Graph {
             node_quantity,
-            max_weight,
+            max_weight: 0,
             adj 
         }
     }
     pub fn add_edge(&mut self, from: usize, to: usize, cost: usize) {
-        self.adj.entry(from).or_insert(HashSet::new()).insert((to, cost));
+        if cost > self.max_weight{
+            self.max_weight = cost;
+        }
+        self.adj[from - 1].insert((to - 1, cost));
     }
     pub fn djikstra_classic_p2p(&mut self, start: usize, goal: usize) -> Option<usize> {
+        let start = start - 1;
+        let goal = goal - 1;
+
         if start == goal {
             return Some(0); 
         }
         let mut queue: BinaryHeap<SearchNode> = BinaryHeap::new();
-        let mut visited: HashSet<usize> = HashSet::new();
-        visited.insert(start);
-        for (id, distance) in self.adj.get(&start).unwrap() {
-            queue.push(SearchNode{id: *id, distance: *distance});
+        let mut visited = vec![false; self.node_quantity];
+        visited[start] = true;
+        for (id, distance) in &self.adj[start] {
+            queue.push(SearchNode{id: *id,  distance: *distance});
         }
         while let Some(current_node) = queue.pop() {
-            visited.insert(current_node.id);
+            visited[current_node.id] = true;
             if current_node.id == goal{
                 return Some(current_node.distance);
             }
             let d = current_node.distance;
-            for (id, dist) in self.adj.get(&current_node.id).unwrap() {
-                if !visited.contains(id){
+            for (id, dist) in &self.adj[current_node.id] {
+                if !visited[*id]{
                     queue.push(SearchNode { id: *id, distance: (dist + d)});
                 }
             }
@@ -64,6 +70,7 @@ impl Graph {
         None
     }
     pub fn djikstra_classic_ss(&mut self, src: usize) -> Vec<Option<usize>>  {
+        let src = src - 1;
         let mut distances = vec![None; self.node_quantity];
         distances[src] = Some(0);
         let mut heap = BinaryHeap::new();
@@ -74,7 +81,7 @@ impl Graph {
                     continue;
                 }
             }
-            for (neighbor, weight) in self.adj.get(&node).unwrap_or(&HashSet::new()) {
+            for (neighbor, weight) in &self.adj[node] {
                 let new_distance = distance + weight;
                 if distances[*neighbor].map_or(true, |d| new_distance < d) {
                     distances[*neighbor] = Some(new_distance);
@@ -85,11 +92,14 @@ impl Graph {
         distances
     }
     pub fn dial_p2p(&mut self, start: usize, goal: usize) -> Option<usize> {
+        let start = start - 1;
+        let goal = goal - 1;
+
         let mut distances = vec![None; self.node_quantity];
         let max_buckets = self.node_quantity * self.max_weight;
-        let mut buckets: Vec<HashSet<usize>> = vec![HashSet::new(); max_buckets];
+        let mut buckets: Vec<Vec<usize>> = vec![Vec::new(); max_buckets];
         distances[start] = Some(0);
-        buckets[0].insert(start);
+        buckets[0].push(start);
         let mut heap = BinaryHeap::new();
         heap.push((Reverse(0), start));
         while let Some((Reverse(dist), node)) = heap.pop() {
@@ -101,27 +111,29 @@ impl Graph {
                     continue;
                 }
             }
-            buckets[dist].remove(&node);
+            buckets[dist].retain(|v| *v != node);
             distances[node] = Some(dist);
-            for (neighbor, weight) in self.adj.get(&node).unwrap() {
-                let new_dist = dist + *weight;
+            for (neighbor, weight) in &self.adj[node] {
+                let new_dist = dist + weight;
                 if let Some(d) = distances.get(*neighbor).and_then(|d| *d) {
                     if new_dist >= d {
                         continue;
                     }
                 }
-                buckets[new_dist].insert(*neighbor);
+                buckets[new_dist].push(*neighbor);
                 heap.push((Reverse(new_dist), *neighbor));
             }
         }
         None
     }
-    pub fn dial_ss(&mut self, src: usize,)  -> Vec<Option<usize>> {
+    pub fn dial_ss(&mut self, src: usize)  -> Vec<Option<usize>> {
+        let src = src - 1;
+
         let mut distances = vec![None; self.node_quantity];
         let max_buckets = self.node_quantity * self.max_weight;
-        let mut buckets: Vec<HashSet<usize>> = vec![HashSet::new(); max_buckets];
+        let mut buckets: Vec<Vec<usize>> = vec![Vec::new(); max_buckets];
         distances[src] = Some(0);
-        buckets[0].insert(src);
+        buckets[0].push(src);
         let mut bucket_idx: usize = 0;
         loop {
             while bucket_idx < max_buckets && buckets[bucket_idx].is_empty() {
@@ -130,16 +142,16 @@ impl Graph {
             if bucket_idx >= max_buckets {
                break; 
             }
-            let v = *buckets[bucket_idx].iter().next().unwrap();
-            buckets[bucket_idx].remove(&v);
-            for (u, w) in self.adj.get(&v).unwrap_or(&HashSet::new()){
+            let v = *buckets[bucket_idx].first().unwrap();
+            buckets[bucket_idx].retain(|x| *x != v);
+            for (u, w) in &self.adj[v] {
                 let alt_dist = distances[v].unwrap_or(usize::MAX) + w;
                 let curr_dist = distances[*u].unwrap_or(usize::MAX);
                 if alt_dist < curr_dist {
                     if curr_dist != usize::MAX {
-                        buckets[alt_dist].remove(u);
+                        buckets[alt_dist].retain(|x| *x != *u);
                     }
-                    buckets[alt_dist].insert(*u);
+                    buckets[alt_dist].push(*u);
                     distances[*u] = Some(alt_dist);
                 }
             }
@@ -147,9 +159,40 @@ impl Graph {
         distances
     }
     pub fn radix_p2p(&mut self, start: usize, goal: usize) -> Option<usize> {
-        todo!()
+        let start = start - 1;
+        let goal = goal - 1;
+
+        if start == goal{
+            return Some(0);
+        }
+        Some(13)
     }
     pub fn radix_ss(&mut self, src: usize,) -> Vec<Option<usize>> {
-        todo!()
+        let src = src - 1;
+
+        let mut distances = vec![None; self.node_quantity];
+        distances[src] = Some(0);
+        distances
     }
+}
+
+
+#[test]
+fn graph_test() {
+    let mut graph = Graph::new(6);
+    graph.add_edge(1, 2, 13);
+    graph.add_edge(1, 3, 0);
+    graph.add_edge(1, 4, 15);
+    graph.add_edge(1, 5, 20);
+    graph.add_edge(2, 4, 5);
+    graph.add_edge(3, 5, 9);
+    graph.add_edge(4, 6, 2);
+    graph.add_edge(5, 6, 4);
+    println!("{:?}", graph);
+    println!("{:?}", graph.djikstra_classic_ss(1));
+    println!("{:?}", graph.djikstra_classic_p2p(1, 5));
+    println!("{:?}", graph.dial_ss(1));
+    println!("{:?}", graph.dial_p2p(1, 5));
+    println!("{:?}", graph.radix_ss(1));
+    println!("{:?}", graph.radix_p2p(1, 2));
 }
--- lista_3/src/main.rs
+++ lista_3/src/main.rs
@@ -1,21 +1,136 @@
-mod djikstra;
-use djikstra::*;
+use std::{io, fs};
+use std::fs::File;
+use std::io::BufRead;
+use std::path::Path;
+
+use dialoguer::{console::Term, Input, theme::ColorfulTheme, Select};
+
+use lib::*;
+
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
 
 fn main() {
-    let mut graph = Graph::new(6, 20);
-    graph.add_edge(0, 1, 13);
-    graph.add_edge(0, 2, 0);
-    graph.add_edge(0, 3, 15);
-    graph.add_edge(0, 4, 20);
-    graph.add_edge(1, 3, 5);
-    graph.add_edge(2, 4, 9);
-    graph.add_edge(3, 5, 2);
-    graph.add_edge(4, 5, 4);
-    println!("{:?}", graph);
-    println!("{:?}", graph.djikstra_classic_ss(0));
-    println!("{:?}", graph.djikstra_classic_p2p(0, 5));
-    println!("{:?}", graph.dial_ss(0));
-    println!("{:?}", graph.dial_p2p(0, 5));
-    //println!("{:?}", graph.radix_ss(0));
-    //println!("{:?}", graph.radix_p2p(0, 2));
+    let input : String = Input::with_theme(&ColorfulTheme::default())
+        .with_prompt("Provide inputs directory path")
+        .interact_text()
+        .unwrap();
+    let paths = fs::read_dir(input).unwrap();
+
+    let graph_choices = vec!["NY", "BAY", "COL", "FLA", "NW", "NE", "CAL", "LKS", "E", "W", "CRT"];
+    let graph_selection = Select::with_theme(&ColorfulTheme::default())
+        .with_prompt("SELECT GRAPH")
+        .items(&graph_choices)
+        .default(0)
+        .interact_on_opt(&Term::stderr())
+        .expect("failed");
+    if graph_selection.is_none() {
+        panic!("User did not select anything")
+    }
+    
+    let challenge_choices = vec!["p2p", "ss"];
+    let challenge_selection = Select::with_theme(&ColorfulTheme::default())
+        .with_prompt("SELECT CHALLENGE")
+        .items(&challenge_choices)
+        .default(0)
+        .interact_on_opt(&Term::stderr())
+        .expect("failed");
+    if challenge_selection.is_none() {
+        panic!("User did not select anything")
+    }
+
+    let alorithm_choices = vec!["basic", "dial's", "radix"];
+    let algorithm_selection = Select::with_theme(&ColorfulTheme::default())
+        .with_prompt("SELECT ALGORITHM")
+        .items(&alorithm_choices)
+        .default(0)
+        .interact_on_opt(&Term::stderr())
+        .expect("failed");
+    if algorithm_selection.is_none() {
+        panic!("User did not select anything")
+    }
+
+    let mut graph_file: String = "".to_string();
+    let mut challenge_file: String = "".to_string();
+
+    for path in paths{
+        let tmp = path.unwrap().path().display().to_string();
+        if tmp.contains(graph_choices[graph_selection.unwrap()]){
+            if tmp.contains(".gr"){
+                graph_file = tmp.clone();
+            }
+            if tmp.contains(challenge_choices[challenge_selection.unwrap()]){
+                challenge_file = tmp.clone();
+            }
+        }
+    }
+    //println!("{:?}", graph_file);
+    //println!("{:?}", challenge_file);
+
+    let mut g = Graph::default();
+    if let Ok(lines) = read_lines(graph_file) {
+        for line in lines.flatten() {
+            if line.starts_with('p') {
+                g = Graph::new(line.split_whitespace().collect::<Vec<&str>>()[2].parse().unwrap());
+            } else if line.starts_with('a') {
+                let tmp = line.split_whitespace().collect::<Vec<&str>>();
+                g.add_edge(tmp[1].parse().unwrap(), tmp[2].parse().unwrap(), tmp[3].parse().unwrap())
+            }
+        }
+    }
+
+    //p2p if 0
+    if challenge_selection.unwrap() == 0 {
+        let mut p2ps: Vec<(usize, usize)> = Vec::new();
+        if let Ok(lines) = read_lines(challenge_file) {
+            for line in lines.flatten() {
+                if line.starts_with('q') {
+                    let tmp = line.split_whitespace().collect::<Vec<&str>>();
+                    p2ps.push((tmp[1].parse().unwrap(), tmp[2].parse().unwrap()));
+                }
+            }
+        }
+        for p2p in p2ps{
+            match algorithm_selection.unwrap() {
+                0 => {
+                    println!("{:?}", g.djikstra_classic_p2p(p2p.0, p2p.1))
+                },
+                1 => {
+                    println!("{:?}", g.dial_p2p(p2p.0, p2p.1))
+                },
+                2 => {
+                    println!("{:?}", g.radix_p2p(p2p.0, p2p.1))
+                },
+                _ => panic!("Something went wrong!")
+            }
+        }
+    } else {
+        let mut ss: Vec<usize> = Vec::new();
+        if let Ok(lines) = read_lines(challenge_file) {
+            for line in lines.flatten() {
+                if line.starts_with('s') {
+                    let tmp = line.split_whitespace().collect::<Vec<&str>>();
+                    ss.push(tmp[1].parse().unwrap());
+                }
+            }
+        }
+        for src in ss{
+            match algorithm_selection.unwrap() {
+                0 => {
+                    println!("{:?}", g.djikstra_classic_ss(src))
+                },
+                1 => {
+                    println!("{:?}", g.dial_ss(src))
+                },
+                2 => {
+                    println!("{:?}", g.radix_ss(src))
+                },
+                _ => panic!("Something went wrong!")
+            }
+        }
+    }
 }
