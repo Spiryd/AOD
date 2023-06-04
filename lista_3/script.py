cd7aef3bbfe2b1e9475db010eff18484a627f346
Spiryd <neumann.maks@gmail.com>
2023-06-04 21:12:34 +0200

    ok

--- lista_3/script.py
+++ lista_3/script.py
@@ -2,5 +2,11 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("data/dijkstra_data.csv")
-df.head()
+df = pd.read_csv("dijkstra_data.csv", header=0, sep=";")
+print(df.value_counts("n"))
+print(df.head())
+df = df.astype({'n': 'int', "time": "float"})
+
+sns.set_style("darkgrid")
+sns.barplot(data=df, x="n", y="time", hue="algo")
+plt.savefig("ok.png")
