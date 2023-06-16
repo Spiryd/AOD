27140cbc3f6e3699f36b01600d7f771732cdcbb2
Spiryd <neumann.maks@gmail.com>
2023-06-16 20:26:22 +0200

    Hopcroft Karp algorithm

--- lista_4/src/lib.rs
+++ lista_4/src/lib.rs
@@ -1,47 +1,148 @@
 use std::collections::VecDeque;
-
 use rand::prelude::*;
 use rand_pcg::Pcg64;
 
+const NIL: usize = 0;
+const INF: usize = usize::MAX;
+
 #[derive(Debug, Clone)]
-pub struct Network {
-    node_quantity: usize,
-    adj: Vec<Vec<u8>>,
+pub struct Bigraph {
+    adj: Vec<Vec<usize>>,
+    n: usize,
+    m: usize,
+}
+
+impl Bigraph {
+    pub fn new(k: u32, i: usize) -> Self {
+        let mut rng: Pcg64 = Pcg64::from_entropy();
+        let [n, m] = [2_usize.pow(k); 2];
+        let mut adj: Vec<Vec<usize>> = vec![Vec::new(); n + 1];
+        let u_nodes: Vec<usize> = (1..=n).collect();
+        for u in 1..=n {
+            for v in u_nodes.choose_multiple(&mut rng, i) {
+                adj[u].push(*v);
+            }
+        }
+        Bigraph { adj, n, m }
+    }
+    pub fn hopcroft_karp(&self) -> usize {
+        let mut pair_u: Vec<usize> = vec![NIL; self.n + 1];
+        let mut pair_v: Vec<usize> = vec![NIL; self.m + 1];
+        let mut dist: Vec<usize> = vec![usize::MAX; self.n + 1];
+        let mut matching: usize = 0;
+        
+        while self.bfs(&mut pair_u, &mut pair_v, &mut dist) {
+            for u in 1..=self.n {
+                if pair_u[u] == NIL && self.dfs(u, &mut pair_u, &mut pair_v, &mut dist) {
+                    matching += 1;
+                }
+            }
+        }
+
+        matching
+    }
+    fn bfs(&self, pair_u: &mut Vec<usize>,  pair_v: &mut Vec<usize>, dist: &mut Vec<usize>) -> bool{
+        let mut queue: VecDeque<usize> = VecDeque::new();
+
+        for u in 1..=self.n {
+            if pair_u[u] == NIL {
+                dist[u] = 0;
+                queue.push_back(u);
+            } else {
+                dist[u] = INF;
+            }
+        }
+        dist[NIL] = INF;
+        while let Some(u) = queue.pop_front() {
+            if dist[u] < dist[NIL] {
+                for v in &self.adj[u] {
+                    if dist[pair_v[*v]] == INF {
+                        dist[pair_v[*v]] = dist[u] + 1;
+                        queue.push_back(pair_v[*v]);
+                    }
+                }
+            }
+        }
+        dist[NIL] != INF
+    }
+    fn dfs(&self, u: usize, pair_u: &mut Vec<usize>, pair_v: &mut Vec<usize>, dist: &mut Vec<usize>) -> bool {
+        if u != NIL {
+            for v in &self.adj[u] {
+                if dist[pair_v[*v]] == dist[u] + 1 {
+                    if self.dfs(pair_v[*v], pair_u, pair_v, dist) {
+                        pair_v[*v] = u;
+                        pair_u[u] = *v;
+                        return true;
+                    }
+                }
+            }
+            dist[u] = INF;
+            return false;
+        }
+        return true;
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Hypercube {
+    pub node_quantity: usize,
+    //adj: Vec<Vec<u8>>,
     cap: Vec<Vec<isize>>
 }
 
-impl Network {
-    pub fn new(node_quantity: usize) -> Self {
-        let adj: Vec<Vec<u8>> = vec![vec![0; node_quantity]; node_quantity];
-        let cap: Vec<Vec<isize>> = vec![vec![0; node_quantity]; node_quantity];
-        Network { node_quantity, adj, cap }
-    }
-    pub fn new_hypercube(n: u32) -> Self {
+impl Hypercube {
+    pub fn new(n: u32) -> Self {
         let mut rng: Pcg64 = Pcg64::from_entropy();
         let node_quantity= 2_usize.pow(n);
         let mut adj: Vec<Vec<u8>> = vec![vec![0; node_quantity]; node_quantity];
         let mut cap: Vec<Vec<isize>> = vec![vec![0; node_quantity]; node_quantity];
         for v in 0..node_quantity {
             for u in 0..node_quantity {
-                if v > u {
-                    if hamming_distance(v, u) == 1 {
-                        adj[v][u] = 1;
-                    }
-                }
-            }
-        }
-        for v in 0..node_quantity {
-            for u in 0..node_quantity {
-                if adj[v][u] == 1 {
+                if v < u && hamming_distance(v, u) == 1 {
+                    adj[v][u] = 1;
                     let l = *vec![hamming_weight(v), hamming_weight(u), rev_hamming_weight(v, n), rev_hamming_weight(u, n)]
-                        .iter()
-                        .max()
-                        .unwrap();
+                    .iter()
+                    .max()
+                    .unwrap();
                     cap[v][u] = rng.gen_range(1..(2_isize.pow(l)));
                 }
             }
         }
-        Network { node_quantity, adj, cap }
+        Hypercube { node_quantity, cap }
+    }
+
+    pub fn edmonds_karp(&self, source: usize, sink: usize) -> isize {
+        let node_quantity = self.node_quantity;
+        let mut resid: Vec<Vec<isize>> = self.cap.clone();
+        let mut parent: Vec<isize> = vec![0; node_quantity];
+        let mut maxflow: isize = 0;
+    
+        while bfs_for_edmonds_karp(source, sink, &mut parent, &mut resid) {
+            let mut path_flow = isize::MAX;
+            let mut v = sink;
+            while v != source {
+                let u = parent[v];
+                path_flow = path_flow.min(resid[u as usize][v as usize]);
+                v = u as usize;
+            }
+    
+            v = sink;
+    
+            while v != source {
+                let u = parent[v];
+                resid[u as usize][v] -= path_flow;
+                resid[v][u as usize] += path_flow;
+                v = u as usize;
+            }
+    
+            maxflow += path_flow;
+        }
+    
+        maxflow
+    }
+
+    pub fn dinic(&self, source: usize, sink: usize) -> isize {
+        todo!()
     }
 }
 
@@ -74,34 +175,23 @@ fn rev_hamming_weight(x: usize, position: u32) -> u32 {
     count
 }
 
-pub fn bfs_for_esmond_karp(network: &Network, source: usize, sink: usize, parent: &mut Vec<isize> ) -> isize {
-    parent.fill(-1);
-    parent[source] = -2;
+fn bfs_for_edmonds_karp(source: usize, sink: usize, parent: &mut Vec<isize>, resid: &mut Vec<Vec<isize>>) -> bool{
+    let mut visited = vec![false; resid.len()];
+    let mut queue: VecDeque<usize> = VecDeque::new();
+    queue.push_back(source);
+    visited[source] = true;
+    parent[source] = -1;
 
-    0
-}
-
-pub fn esmond_karp(network: &Network, source: usize, sink: usize) -> isize {
-    let node_quantity = network.node_quantity;
-    let mut resid: Vec<Vec<isize>> = network.cap.clone();
-    let mut parent: Vec<isize> = vec![0; node_quantity];
-    let mut maxflow = 0;
-    let mut new_flow: isize;
-    while { new_flow = bfs_for_esmond_karp(network, source, sink, &mut parent); new_flow > 0 } {
-        maxflow += new_flow;
-        let mut cur = sink;
-        while cur != source {
-            let prev = parent[cur] as usize;
-            resid[prev][cur] -= new_flow;
-            resid[cur][prev] += new_flow;
-            cur = prev;
+    while let Some(u) = queue.pop_front() {
+        for v  in 0..resid.len() {
+            if !visited[v] && resid[u][v] > 0 {
+                queue.push_back(v);
+                parent[v] = u as isize;
+                visited[v] = true;
+            }
         }
     }
-    maxflow
-}
-
-pub fn dinic(network: &Network) {
-    todo!()
+    visited[sink]
 }
 
 #[cfg(test)]
@@ -125,6 +215,23 @@ mod tests {
 
     #[test]
     fn hyper_cube_test(){
-        Network::new_hypercube(4);
+        Hypercube::new(4);
+    }
+
+    #[test]
+    fn edmonds_karp_test(){
+        let n = Hypercube::new(4);
+        n.edmonds_karp(0, 15);
+    }
+
+    #[test]
+    fn bigraph_test() {
+        Bigraph::new(6, 4);
+    }
+
+    #[test]
+    fn hopcroft_karp_test() {
+        let bigraph = Bigraph::new(6, 4);
+        bigraph.hopcroft_karp();
     }
 }
--- lista_4/src/main.rs
+++ lista_4/src/main.rs
@@ -1,7 +1,63 @@
+use dialoguer::{console::Term, theme::ColorfulTheme, Select};
+use std::time::Instant;
+use std::fs::File;
+use std::io::Write;
 
 use lib::*;
 
 fn main() {
-    let hyper_cube = Network::new_hypercube(2);
-    println!("{:?}", hyper_cube);
+    let choices = ["Edmonds-Karp Algorithm", "Maximum Cardinality Matching", "Dinic's Algorithm", "ALL"];
+    let selection = Select::with_theme(&ColorfulTheme::default())
+        .with_prompt("Collect Data On:")
+        .items(&choices)
+        .default(0)
+        .interact_on_opt(&Term::stderr())
+        .expect("failed");
+    if selection.is_none() {
+        panic!("User did not select anything")
+    }
+
+    match selection.unwrap() {
+        0 => collect_edmonds_karp_data(),
+        1 => collect_mcm_data(),
+        2 => collect_dinic_data(),
+        3 => {
+            collect_edmonds_karp_data();
+            collect_mcm_data();
+            collect_dinic_data();
+        },
+        _ => panic!("somthing went wrong"),
+    }   
+}
+
+fn collect_edmonds_karp_data() {
+    let mut file = File::create("./data/edmonds_karp.csv").unwrap();
+    file.write_all(b"n;flow;time\n").unwrap();
+    for n in 1..=16 {
+        let hyper_cube = Hypercube::new(n);
+        println!("{n}-dimensional Hypercube created");
+        for _ in 0..10 {
+            let start = Instant::now();
+            let max_flow = hyper_cube.edmonds_karp(0,  2_usize.pow(n) - 1);
+            let duration =  start.elapsed().as_secs_f64();
+            file.write_all(format!("{n};{max_flow};{duration}\n").as_bytes()).unwrap();
+        }
+    }
+}
+
+fn collect_mcm_data() {
+    let mut file = File::create("./data/mcm.csv").unwrap();
+    file.write_all(b"k;i;size;time\n").unwrap();
+    for k in 3..=10 {
+        for i in 1..=k {
+            let bigraph = Bigraph::new(k, i as usize);
+            for _ in 0..100{
+                println!("{:?}", bigraph.hopcroft_karp());
+            }
+        }
+    }
+}
+
+fn collect_dinic_data() {
+    todo!()
 }
